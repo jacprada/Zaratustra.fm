db5d12096b7353f60189ae99ca56320cecb26c7a
jacprada <jacopo.prada@gmail.com>
2015-08-04 11:51:19 +0100

    set up for custom error pages done and tested

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'errors/file_not_found'
+
+  get 'errors/unprocessable'
+
+  get 'errors/internal_server_error'
+
   resources :albums
   resources :playlists
   resources :users
@@ -7,4 +13,8 @@ Rails.application.routes.draw do
   root "static#home"
   get "/login", to: "sessions#new"
   delete "/logout", to: "sessions#destroy"
+
+  match '/404', to: 'errors#file_not_found', via: :all
+  match '/422', to: 'errors#unprocessable', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
 end
